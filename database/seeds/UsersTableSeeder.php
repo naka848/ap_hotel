620c2a213629b1aae3b11018ff0b440cf237d310
naka848 <n.moeko1966@gmail.com>
2021-10-27 10:34:47 +0900

    usersテーブル作成やり直し

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,21 +5,26 @@ use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
-
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
         $param = [
             'name' => 'hikaru',
             'address' => 'Onomichi-shi, Hiroshima-ken',
             'tel' => '12-3456',
-        ];
-        DB::table('users')->insert($param);
-
-        $param = [
-            'name' => 'akira',
-            'address' => 'Chiyoda-ku, Tokyo-to',
-            'tel' => '78-9101',
-        ];
-        DB::table('users')->insert($param);
+            ];
+            DB::table('users')->insert($param);
+            
+            
+            $param = [
+                'name' => 'akira',
+                'address' => 'Chiyoda-ku, Tokyo-to',
+                'tel' => '78-9101',
+            ];
+            DB::table('users')->insert($param);
     }
 }
