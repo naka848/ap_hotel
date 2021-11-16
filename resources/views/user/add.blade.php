25db6f920114b0518008edbaeae3c39f071aca4e
naka848 <n.moeko1966@gmail.com>
2021-11-16 13:16:39 +0900

    リンク更新

--- resources/views/user/add.blade.php
+++ resources/views/user/add.blade.php
@@ -14,7 +14,13 @@
         <li><a href="{{ url('reservation/add') }}">予約</a></li>
         <li><a href="{{ url('user') }}">利用者一覧</a></li>
         <li><a href="{{ url('reservation') }}">予約一覧</a></li>
+        <li><a href="{{ url('reservation_detail') }}">予約詳細一覧</a></li>
         <li><a href="{{ url('user/list') }}">利用者／予約一覧</a></li>
+        <li><a href="{{ url('reservation/list') }}">予約／利用者一覧</a></li>
+        <li><a href="{{ url('reservation/list2') }}">予約／部屋一覧（多対多）</a></li>
+        <li><a href="{{ url('room') }}">部屋一覧</a></li>
+        <li><a href="{{ url('room_type') }}">部屋種別一覧</a></li>
+        <li><a href="{{ url('room_type/list') }}">部屋／部屋種別一覧</a></li>
     </ul>
 </nav>
 <br>
