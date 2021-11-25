af5e4e5ebae54ffb1ccfadb2a4e1c977249275d5
naka848 <n.moeko1966@gmail.com>
2021-11-26 08:51:15 +0900

    NULL判定できた！あとは日付の指定！！

--- resources/views/room/list.blade.php
+++ resources/views/room/list.blade.php
@@ -17,25 +17,56 @@
             <th>部屋種別</th>
             <th>宿泊可能人数</th>
         </tr>
+        {{-- @foreach($items as $item)
+            <tr>
+                <td>{{ $item->room_id }}</td>
+                <td>{{ $item->room_number }}</td>
+                <td>{{ $item->room_type_id }}</td>
+                <td>{{ $item->room_type->room_type }}</td>
+                <td>{{ $item->room_type->capacity }}</td>
+            </tr>
+        @endforeach --}}
+        
+        {{-- これだと２個めのFOREACHで予約されてる部屋のデータに絞られてしまう
         @foreach($items as $item)
-            {{-- @if ($item が一致する日付があれば)continue~ --}}
-        <tr>
-            <td>{{ $item->room_id }}</td>
-            <td>{{ $item->room_number }}</td>
-            <td>{{ $item->room_type_id }}</td>
-            <td>{{ $item->room_type->room_type }}</td>
-            <td>{{ $item->room_type->capacity }}</td>
-
             @foreach ($item->reservations as $reservation)
+                @if(empty($reservation->check_in_day))
+                    <tr>
+                        <td>{{ $item->room_id }}</td>
+                        <td>{{ $item->room_number }}</td>
+                        <td>{{ $item->room_type_id }}</td>
+                        <td>{{ $item->room_type->room_type }}</td>
+                        <td>{{ $item->room_type->capacity }}</td>
+                        <td>{{ $item }}</td>
+                        <td>{{ $reservation }}</td>
+                    </tr>
+                @endif
+            @endforeach
+        @endforeach --}}
+
+        @foreach($items as $item)   
+            @if(empty($item->reservations->first()->check_in_day))
+                <tr>
+                    <td>{{ $item->room_id }}</td>
+                    <td>{{ $item->room_number }}</td>
+                    <td>{{ $item->room_type_id }}</td>
+                    <td>{{ $item->room_type->room_type }}</td>
+                    <td>{{ $item->room_type->capacity }}</td>
+                    <td>{{ $item }}</td>
+                </tr>
+            @endif
+        @endforeach
+
+    </table>
+
+                {{-- 予約日の指定（メモ） --}}
+                {{-- @foreach ($item->reservations as $reservation)
             <td>{{ $reservation->check_in_day }}</td>
             <td>{{ $reservation->check_out_day }}</td>
-            @endforeach
+            @endforeach --}}
 
-            {{-- これだと空のデータを含むので表示できなさそう
+                        {{-- これだと空のデータを含むので表示できなさそう
             <td>{{ $item->reservations->first()->check_in_day }}</td> --}}
-        </tr>
-        @endforeach
-    </table>
     
 @endsection
 
